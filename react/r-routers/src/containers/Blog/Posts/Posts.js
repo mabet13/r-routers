1477def91d4ab24ca4a3a972e3e5ef55e9a8df48
mabet13 <mika@gattovi.com>
2019-08-05 13:43:58 +0200

    nested routes

--- react/r-routers/src/containers/Blog/Posts/Posts.js
+++ react/r-routers/src/containers/Blog/Posts/Posts.js
@@ -1,9 +1,10 @@
 import React, {Component} from  'react';
 import axios from '../../../axios';
-import { Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
 import './Posts.css';
+import FullPost from '../FullPost/FullPost';
 
 class Posts extends Component {
     state = {
@@ -12,7 +13,7 @@ class Posts extends Component {
 
     postSelectedHandler = (id) => {
         //this.setState({selectedPostId: id});
-        this.props.history.push({pathname: '/' + id});
+        this.props.history.push('/posts/' + id);
     }
 
     componentDidMount () {
@@ -52,10 +53,14 @@ class Posts extends Component {
         }
 
         return (
-            <section className="Posts">
-                {posts}
-            </section>
-        )}
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <Route path={this.props.match.url + '/:id'} exact component={FullPost} />
+            </div>
+        )
+    }
 }
 
 export default Posts;

44a10291a4b26b88e415a7c00552e4a53ffb085e
mabet13 <mika@gattovi.com>
2019-08-05 15:09:44 +0200

    lazy loading

--- react/r-routers/src/containers/Blog/NewPost/NewPost.js
+++ react/r-routers/src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 import './NewPost.css';
 
@@ -7,7 +8,8 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Mika',
+        submitted: false
     }
 
     componentDidMount () {
@@ -23,12 +25,19 @@ class NewPost extends Component {
         axios.post('/posts', data)
             .then(response => {
                 console.log(response);
+                this.props.history.replace('/posts');
+                //this.setState({submitted: true});
             });
     }
 
     render () {
+        let redirect = null;
+        if (this.state.submitted) {
+            redirect = <Redirect to ="/posts" />;
+        }
         return (
             <div className="NewPost">
+                {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
@@ -36,8 +45,10 @@ class NewPost extends Component {
                 <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                    <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
+                    <option value="Max">Mika</option>
+                    <option value="Paula">Paula</option>/
+                    <option value="Tiago">Tiago</option>/
+                    <option value="Sarah">Sarah</option>/
                 </select>
                 <button onClick={this.postDataHandler}>Add Post</button>
             </div>

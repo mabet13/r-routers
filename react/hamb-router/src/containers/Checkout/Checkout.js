db6bffebcc2802053db61f6f5a2b41b480ad69f7
mabet13 <mika@gattovi.com>
2019-08-21 11:23:41 +0200

    adding more and more and more complexity

--- react/hamb-router/src/containers/Checkout/Checkout.js
+++ react/hamb-router/src/containers/Checkout/Checkout.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
+//import * as actions from '../../store/actions/index';
 
 class Checkout extends Component {
 
@@ -18,8 +19,10 @@ class Checkout extends Component {
     render () {
         let summary = <Redirect to="/" />
         if (this.props.ings) {
+            const purchaseRedirect = this.props.purchased ? <Redirect to="/" /> : null;
             summary = (
                 <div>
+                    {purchaseRedirect}
                     <CheckoutSummary
                         ingredients={this.props.ings}
                         checkoutCancelled={this.checkoutCancelledHandler}
@@ -37,8 +40,9 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.burgerBuilder.ingredients
-    }
+        ings: state.burgerBuilder.ingredients,
+        purchased: state.order.purchased
+    };
 };
 
 export default connect(mapStateToProps)(Checkout);
